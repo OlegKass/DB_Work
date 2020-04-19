7500e6f791ee878d656eeeba4152bdceef6bdc92
OlegKass <44788664+OlegKass@users.noreply.github.com>
2020-04-19 22:44:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import cx_Oracle
+
+username = 'Prac'
+password = '139'
+dsn = 'localhost/xe'
+
+connection = cx_Oracle.connect(username, password, dsn)
+cursor = connection.cursor()
+
+# Запит 1 - Категорія(предметна область) - число. Вивести категорію та кількість лауретів цієї області.
+query = '''
+Select category_name, count(*)
+From categories
+Group By category_name
+'''
+
+print('Запит 1')
+cursor.execute(query)
+row = cursor.fetchone()
+while row:
+    print(row)
+    row = cursor.fetchone()
+print('\n\n')
+
+# Запит 2 - Країна народження - відсоток лауреатів цієї країни відносно решти.
+query = '''
+SELECT birth_country_name AS event, ROUND(COUNT(birth_country_name) * 100 / (SELECT COUNT(*) FROM Laureat), 2)
+AS percentage
+FROM Birth inner join birth_city
+using(birth_city_name)
+join birth_country
+using(birth_country_name)
+GROUP BY birth_country_name
+'''
+
+print('Запит 2')
+cursor.execute(query)
+row = cursor.fetchone()
+while row:
+    print(row)
+    row = cursor.fetchone()
+print('\n\n')
+
+# Запит 3 -  Динаміка кількості лауреатів по годах.
+query = '''
+SELECT reward_year, COUNT(*) 
+from Laureat
+Group by reward_year
+'''
+
+print('Запит 3')
+cursor.execute(query)
+row = cursor.fetchone()
+while row:
+    print (row)
+    row = cursor.fetchone()
+
+cursor.close()
+connection.close()
