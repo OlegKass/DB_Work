398e23186f59610da492ac63a5cf93a0c4a54892
OlegKass <44788664+OlegKass@users.noreply.github.com>
2020-05-07 21:03:48 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -2,7 +2,7 @@
 Select category_name, count(*)
 From categories inner join 
 Laureat USING(category_name)
-Group By category_name
+Group By category_name;
 -- 2 ?????
 SELECT birth_country_name AS event, ROUND(COUNT(birth_country_name) * 100 / (SELECT COUNT(*) FROM Laureat), 2)
 AS percentage
@@ -10,8 +10,8 @@ FROM Birth inner join birth_city
 using(birth_city_name)
 join birth_country
 using(birth_country_name)
-GROUP BY birth_country_name
+GROUP BY birth_country_name;
 -- 3 ?????
 SELECT reward_year, COUNT(*) 
 from Laureat
-Group by reward_year
+Group by reward_year;
